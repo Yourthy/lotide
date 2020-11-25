9f57343070ad9562c2e41113dc20e2357b823156
Yourthy <christopher.t.yourth@gmail.com>
2020-11-25 04:16:34 +0000

    added console.log instead of return values

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,13 +1,18 @@
-const eqArrays = function(array1,array2) {
+const assertArraysEqual = function(array1,array2) {
     if(array1.length !== array2.length) {
-      return false;
+      console.log(`Assertion Failed: ${array1.length} !== ${array2.length}`);
+      return;
     } else {
       for(let i = 0; i < array1.length; i++){
         if(array1[i] !== array2[i]){
-          return "These arrays are not equal!";
+          console.log(`Assertion Failed: ${array1} !== ${array2} `);
+          return
         }
       }
-    }return "These Arrays are equal!";
+    }console.log(`Assertion Passed: ${array1} === ${array2} `);
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual([1,2,3,4],[1,2,3,4]);
+assertArraysEqual([1,2,3,4],['1','2','3','4']);
+assertArraysEqual([1,2,3,4],['1','2','3']);
