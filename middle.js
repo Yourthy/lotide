8dcca1adfbef1cb226b093af5e86052d196083a1
Yourthy <christopher.t.yourth@gmail.com>
2020-11-30 22:12:31 +0000

    refactored by referenceing assertion files

--- middle.js
+++ middle.js
@@ -1,18 +1,13 @@
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
-
-  const middle = function(array){
+  const middle = function(array) {
     let result = [];
-    if(array.length <= 2){
-        return result;
-        
-    }else if(array.length % 2 === 0){
-        result.push(array[array.length/2 -1], array[array.length/2]);
-
-    }else{
-        result.push(array[Math.round((array.length - 1) / 2)]);
+    if (array.length <= 2) {
+      return result;
+    } else if (array.length % 2 === 0) {
+      result.push(array[array.length / 2 - 1], array[array.length / 2]);
+    } else {
+      result.push(array[Math.round((array.length - 1) / 2)]);
     }
     return result;
-}
+  };
 
 module.exports = middle;
