ddf27644084602ad35b0eca79180938a1a86d17a
Yourthy <christopher.t.yourth@gmail.com>
2020-11-30 22:50:54 +0000

    non commited files

--- test/headtest.js
+++ test/headtest.js
@@ -1,7 +1,24 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
 const head = require('../head');
 
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+//mocha chai assertions
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 'hello' for ['Hello', 'Lighthouse', 'Labs']", () =>{
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns '[]' for 'Hello", () =>{
+      assert.isUndefined(head([], "hello"))
+  });
+
+});
