abcb9e19c955394fe504411ac8c0bf0764eaf58d
Yourthy <christopher.t.yourth@gmail.com>
2020-11-30 21:05:56 +0000

    test code for the assertEqual file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Chris", "Chris");
+assertEqual("chris", "Chris");
+assertEqual(5, 222);
