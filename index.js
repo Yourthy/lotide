bd435da071e231756bec936111eceed363e8b65c
Yourthy <christopher.t.yourth@gmail.com>
2020-12-01 04:01:16 +0000

    updated with object shorthand

---
+++ index.js
@@ -0,0 +1,33 @@
+const head = require("./head"); //√
+const eqArrays = require("./eqArrays"); //√
+const assertEqual = require("./assertArraysEqual"); //√
+const assertArraysEqual = require("./assertArraysEqual"); //√
+const middle = require("./middle"); //√
+const tail = require("./tail"); //√
+const countLetters = require("./countLetters"); //√
+const countOnly = require("./countOnly"); 
+const findKey = require("./findKey"); 
+const letterPositions = require("./letterPositions");
+const map = require("./map"); 
+const takeUntil = require("./takeUntil"); 
+const without = require("./without"); 
+const assertObjectsEqual = require("./assertArraysEqual"); 
+const eqObjects = require("./eqObjects");
+
+module.exports = {
+  head,
+  tail,
+  countLetters,
+  middle,
+  findKey,
+  letterPositions,
+  countOnly,
+  map,
+  takeUntil,
+  without,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  eqObjects,
+  eqArrays,
+};
